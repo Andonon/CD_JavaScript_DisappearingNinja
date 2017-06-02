5fcc60a619bbfdd404ff162e74e6d35d1decdac7
Troy Center <troycenter1@gmail.com>
2017-06-02 09:04:22 -0700

    finsihed html css and js files. Done

--- js/ninjas.js
+++ js/ninjas.js
@@ -1,5 +1,11 @@
 "use strict";
     $(document).ready(function(){ 
-        
+        $("img").click(function(){
+            var imgid = $(this).attr("id")
+            $("Li.imgid").css("border","none")
+            $(this).fadeOut();
+        });        
+        $("#restorebutton").click(function(){
+            $("img").show();
+        });        
     });    
-    
